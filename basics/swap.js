5925104cc9b5af4bf5900eddb5dd4a0db73bacdc
DevSecPrime <ranaaryan620@gmail.com>
2025-08-12 21:12:27 +0530

    feat: swap 2 numbers: Method 1

---
+++ basics/swap.js
@@ -0,0 +1,13 @@
+//Swap two numbers - 3 methods
+
+//Method 1 ---> Using extra variable
+let a = 10;
+let b = 20;
+let c;
+
+c = a; // c= 10, a = 10;
+a = b; //a=20, b=20;
+b = c; //b=10
+
+console.log("After swapping value of a: ", a);
+console.log("After swapping value of b: ", b);
