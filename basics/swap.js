4786fbac7d9dffd3603bb4590417ebc7cec62d76
DevSecPrime <ranaaryan620@gmail.com>
2025-08-12 21:22:17 +0530

    feat: swap 2 numbers: Method 2

--- basics/swap.js
+++ basics/swap.js
@@ -5,9 +5,17 @@ let a = 10;
 let b = 20;
 let c;
 
-c = a; // c= 10, a = 10;
-a = b; //a=20, b=20;
-b = c; //b=10
+// c = a; // c= 10, a = 10;
+// a = b; //a=20, b=20;
+// b = c; //b=10
+
+// console.log("After swapping value of a: ", a);
+// console.log("After swapping value of b: ", b);
+
+//Method 2 ---> Using Mathematics --> no need to use extra variable
+a = a + b; // 10+20=30 new value of a = 30
+b = a - b; //30-20=10 new value of b = 10
+a = a - b; //a-b --> 30-10 = 20 new value of a = 20
 
 console.log("After swapping value of a: ", a);
 console.log("After swapping value of b: ", b);
