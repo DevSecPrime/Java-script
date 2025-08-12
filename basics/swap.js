6a3ec49ab45bd82016baf05925a09d09f3beb7bd
DevSecPrime <ranaaryan620@gmail.com>
2025-08-12 21:30:55 +0530

    feat: swap 2 numbers: Method 3

--- basics/swap.js
+++ basics/swap.js
@@ -5,6 +5,10 @@ let a = 10;
 let b = 20;
 let c;
 
+console.log("Before swapping:");
+console.log("a =", a); // 5
+console.log("b =", b);
+
 // c = a; // c= 10, a = 10;
 // a = b; //a=20, b=20;
 // b = c; //b=10
@@ -13,9 +17,16 @@ let c;
 // console.log("After swapping value of b: ", b);
 
 //Method 2 ---> Using Mathematics --> no need to use extra variable
-a = a + b; // 10+20=30 new value of a = 30
-b = a - b; //30-20=10 new value of b = 10
-a = a - b; //a-b --> 30-10 = 20 new value of a = 20
+//a = a + b; // 10+20=30 new value of a = 30
+//b = a - b; //30-20=10 new value of b = 10
+//a = a - b; //a-b --> 30-10 = 20 new value of a = 20
 
-console.log("After swapping value of a: ", a);
-console.log("After swapping value of b: ", b);
+//console.log("After swapping value of a: ", a);
+//console.log("After swapping value of b: ", b);
+
+//Method 3 ---> Using Array destructuring method
+[a, b] = [b, a]; //Swap using array destructuring
+//Print after swapping
+console.log("After swapping:");
+console.log("a =", a); // 10
+console.log("b =", b); // 5
