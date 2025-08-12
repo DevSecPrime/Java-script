fdeb696122c36625b8d2e69bbf8f95229783250f
DevSecPrime <ranaaryan620@gmail.com>
2025-08-12 21:36:39 +0530

    feat: swap 2 numbers

--- basics/swap.js
+++ basics/swap.js
@@ -9,24 +9,27 @@ console.log("Before swapping:");
 console.log("a =", a); // 5
 console.log("b =", b);
 
-// c = a; // c= 10, a = 10;
-// a = b; //a=20, b=20;
-// b = c; //b=10
+c = a; // c= 10, a = 10;
+a = b; //a=20, b=20;
+b = c; //b=10
 
-// console.log("After swapping value of a: ", a);
-// console.log("After swapping value of b: ", b);
+console.log("Method 1");
+console.log("After swapping value of a: ", a);
+console.log("After swapping value of b: ", b);
 
 //Method 2 ---> Using Mathematics --> no need to use extra variable
-//a = a + b; // 10+20=30 new value of a = 30
-//b = a - b; //30-20=10 new value of b = 10
-//a = a - b; //a-b --> 30-10 = 20 new value of a = 20
+a = a + b; // 10+20=30 new value of a = 30
+b = a - b; //30-20=10 new value of b = 10
+a = a - b; //a-b --> 30-10 = 20 new value of a = 20
 
-//console.log("After swapping value of a: ", a);
-//console.log("After swapping value of b: ", b);
+console.log("Method 2");
+console.log("After swapping value of a: ", a);
+console.log("After swapping value of b: ", b);
 
 //Method 3 ---> Using Array destructuring method
 [a, b] = [b, a]; //Swap using array destructuring
 //Print after swapping
+console.log("Method 3");
 console.log("After swapping:");
 console.log("a =", a); // 10
 console.log("b =", b); // 5
