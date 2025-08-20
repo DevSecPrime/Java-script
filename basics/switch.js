90bbf74da0627083a9455b5fbe8c71bcfbedfed4
DevSecPrime <ranaaryan620@gmail.com>
2025-08-20 21:16:47 +0530

    refactor: switch

---
+++ basics/switch.js
@@ -0,0 +1,13 @@
+///normal switch case
+const day = 4;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  default:
+    console.log("Invalid day");
+}
