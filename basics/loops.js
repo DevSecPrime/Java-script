2f15158343fe20b319a27cfb41e6696bff2a5d10
DevSecPrime <ranaaryan620@gmail.com>
2025-08-21 20:53:05 +0530

    loop: for with special case

---
+++ basics/loops.js
@@ -0,0 +1,21 @@
+//for
+for (let i = 1; i < 10; i++) {
+  console.log("value of i: ", i);
+}
+//Special interview cases
+
+// for (let i = 1; i < 10; ) { //this will go infinite ---- valid code
+//   console.log("value of i: ", i);
+// }
+
+// for (let i = 1;; i++) { //this will also go infinite ----- valid code
+//   console.log("value of i: ", i);
+// }
+
+// for (;;) {  //this will also go infinite ----- valid code
+//   console.log("hello world");
+// }
+
+// for (;){ //Invalid code
+//    console.log("hello world");
+// }
