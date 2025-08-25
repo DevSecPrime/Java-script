7a55dc13bada42047a1dd6dc41ae292f97ea710f
DevSecPrime <ranaaryan620@gmail.com>
2025-08-25 21:12:50 +0530

    feat: prime number

--- basics/prime.js
+++ basics/prime.js
@@ -10,13 +10,29 @@ if ([null, "null", "", "undefined"].includes(input)) {
   if (isNaN(input)) {
     console.log("Invalid input");
   } else {
-    let isPrime = true;
-    for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
-      if (n % i === 0) {
-        isPrime = false;
-        break;
-      }
-    }
-    console.log(isPrime);
+    //Correct but not efficient
+    // let isPrime = true;
+    // for (let i = 2; i <= Math.floor(n / 2); i++) {
+    //   if (n % i === 0) {
+    //     isPrime = false;
+    //     break;
+    //   }
+    // }
+    // console.log(isPrime);
+
+    console.log(checkPrime(n));
   }
+}
+
+//Special method --- time complexity o(n/2)
+function checkPrime(n) {
+  if (n <= 1) return false;
+  if (n === 2) return true;
+  if (n % 2 === 0) return false;
+  for (let i = 3; i <= Math.floor(Math.sqrt(n)); i += 2) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
