a4b6a46c23d980f2a2a5fca9af057a20af413026
DevSecPrime <ranaaryan620@gmail.com>
2025-09-18 21:13:52 +0530

    feat: reflected pyramid

--- pattern/pyramids.js
+++ pattern/pyramids.js
@@ -1,4 +1,4 @@
-//Single pyramid
+// pyramids
 
 // Enter the value of n: 5.
 // n:  5
@@ -7,12 +7,29 @@
 // * * *
 // * * * *
 // * * * * *
+
 const prompt = require("prompt-sync")();
 let n = Number(prompt("Enter the value of n: "));
 console.log("n: ", n);
 
 for (let i = 1; i <= n; i++) {
   for (let j = 1; j <= i; j++) {
+    process.stdout.write("* ");
+  }
+  console.log();
+}
+
+console.log();
+console.log();
+console.log("reflected pyramid");
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+for (let i = 1; i <= n; i++) {
+  for (let j = 5; j >= i; j--) {
     process.stdout.write("* ");
   }
   console.log();
