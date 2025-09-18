d48eae27014bf942c22ece29eb7280822bcb7a2c
DevSecPrime <ranaaryan620@gmail.com>
2025-09-18 20:44:03 +0530

    feat: sigle-pyramid

---
+++ pattern/single-pyramid.js
@@ -0,0 +1,19 @@
+//Single pyramid
+
+// Enter the value of n: 5.
+// n:  5
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+const prompt = require("prompt-sync")();
+let n = Number(prompt("Enter the value of n: "));
+console.log("n: ", n);
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= i; j++) {
+    process.stdout.write("* ");
+  }
+  console.log();
+}
